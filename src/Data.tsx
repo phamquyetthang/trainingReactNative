fdad34c3b1856d514e27ee2071febc2d72a0cc98
phamquyetthang <17021033@vnu.edu.vn>
2020-10-01 09:29:31 +0700

    xong login

---
+++ src/Data.tsx
@@ -0,0 +1,15 @@
+interface person{
+    email: string,
+    pass: string,
+}
+const ACC: person[] = [
+    {
+        email: "phuclong@gmail.com",
+        pass: "1223"
+    },
+    {
+        email: "phucvinh@gmail.com",
+        pass: "2212"
+    }
+]
+export default ACC;
---
+++ src/screens/ListUser.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View} from 'react-native';
+
+const ListUser: React.FC = (): React.ReactElement => {
+  
+  return (
+    <View>
+      <Text>Table</Text>
+    </View>
+  );
+};
+
+export default ListUser;
